82492ee6cc4341e9ea73b85b680c3bc8fb5a6191
jamesli <jamespeilunli@gmail.com>
2025-06-02 15:17:27 -0700

    Add solution to lesson 3 shooter task

--- tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
+++ tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -5,16 +5,17 @@ public class ShooterIOSim implements ShooterIO {
   // Usually this would be in a Constants file, we have it here for convenience
   private static final double MAX_RPM = 6000.0;
 
-  // TODO: Add a private field `simulatedMotorPercent` to store the simulated motor percentage
+  // Store the simulated motor percentage
+  private double simulatedMotorPercent = 0.0;
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
-    // TODO: Simulate motorRPM by multiplying the stored simulatedMotorPercent by the max RPM
-    inputs.motorRPM = 0.0; // Placeholder - replace 0.0
+    // Simulate motor RPM
+    inputs.motorRPM = simulatedMotorPercent * MAX_RPM;
   }
 
   @Override
   public void setMotorPercentOutput(double percent) {
-    // TODO: Store the given `percent` in your simulated motor percentage field
+    simulatedMotorPercent = percent;
   }
 }
