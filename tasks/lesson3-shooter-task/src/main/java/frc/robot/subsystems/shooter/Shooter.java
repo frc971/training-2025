042c1e812352f10fba97d3546b907425a159f192
Rajan Hensley <rajan@MacBook-Air-45.local>
2025-09-19 10:52:26 -0700

    Revert "Implement basic Shooter subsystem with IO pattern"
    This reverts commit 0b0318d4017c26547636a291de37c56443a6a0e8.

--- tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -4,28 +4,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // Main Shooter subsystem logic
 public class Shooter extends SubsystemBase {
-  private ShooterIO shooterIO;
-  private ShooterIO.ShooterIOInputs inputs =  new ShooterIO.ShooterIOInputs();
+  // TODO: Add a private field for your ShooterIO instance
+  // TODO: Add a private field for your ShooterIOInputs instance and initialize it
 
-  public Shooter(ShooterIO io) {
-    this.shooterIO = io;
-  }
+  // TODO: Create the constructor that takes a ShooterIO instance as a parameter
+  // public Shooter(ShooterIO io) { ... }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    shooterIO.updateInputs(inputs);
-    System.out.println(getCurrentRPM());
+    // TODO: Call the updateInputs method on your ShooterIO instance
   }
 
+  // TODO: Add a public method to spin the shooter motor at a given percentage output
   // public void spin(double percent) { ... }
-  public void spin(double percent) {
-    shooterIO.setMotorPercentOutput(percent);
-  }
+
+  // TODO: Add a public method to stop the shooter motor
   // public void stop() { ... }
-  public void stop() {
-    shooterIO.setMotorPercentOutput(0.0);
-  }
-  public double getCurrentRPM() {
-    return inputs.motorRPM;
-  }
+
+  // TODO: Add a public method to return the current motor RPM
+  // public double getCurrentRPM() { ... }
 }
--- tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
+++ tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -5,16 +5,16 @@ public class ShooterIOSim implements ShooterIO {
   // Usually this would be in a Constants file, we have it here for convenience
   private static final double MAX_RPM = 6000.0;
 
-  private double simulatedMotorPercent;
-
+  // TODO: Add a private field `simulatedMotorPercent` to store the simulated motor percentage
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
-    inputs.motorRPM = simulatedMotorPercent*MAX_RPM;
+    // TODO: Simulate motorRPM by multiplying the stored simulatedMotorPercent by the max RPM
+    inputs.motorRPM = 0.0; // Placeholder - replace 0.0
   }
 
   @Override
   public void setMotorPercentOutput(double percent) {
-    simulatedMotorPercent = percent;
+    // TODO: Store the given `percent` in your simulated motor percentage field
   }
 }
